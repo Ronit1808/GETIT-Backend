7086ba71ca2b602df89f328cde3291aebc6d092e
Ronit Gupta <ronit1808@gmail.com>
2024-12-10 16:37:29 +0530

    changes to settings

--- getit/settings.py
+++ getit/settings.py
@@ -142,7 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE= "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE= "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = 'img/'
 MEDIA_ROOT = BASE_DIR/"media"
