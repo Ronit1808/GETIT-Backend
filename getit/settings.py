d3603d2f05bd4e70f985acf858d599e9be22ba42
Ronit Gupta <ronit1808@gmail.com>
2024-12-10 20:12:50 +0530

    added changes to url pattern

--- getit/settings.py
+++ getit/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-#
+#, default=False, cast=bool
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =  config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['getit-backend-95de.onrender.com' , '127.0.0.1']
 
@@ -143,9 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-if not DEBUG:
-    STATIC_ROOT = BASE_DIR / "staticfiles"
-    STATICFILES_STORAGE= "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE= "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/"media"
 
