87e4f27e8ac46e3270fc9cf923bd58676b786aca
Ronit Gupta <ronit1808@gmail.com>
2024-12-10 16:24:43 +0530

    urls changed

--- getit/urls.py
+++ getit/urls.py
@@ -25,8 +25,8 @@ from rest_framework_simplejwt.views import (TokenObtainPairView ,TokenRefreshVie
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("/", include('app.urls')), 
-    path("/", RedirectView.as_view(url='products/', permanent=True)), 
+    path("", include('app.urls')), 
+    path("", RedirectView.as_view(url='products/')), 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
