0ce972f4891d96693d3f5c5f8c21f804db35e8ca
Ronit Gupta <ronit1808@gmail.com>
2025-01-21 13:45:25 +0530

    changes to settings

--- getit/settings.py
+++ getit/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =  config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['getit-backend-95de.onrender.com' , '127.0.0.1']
+ALLOWED_HOSTS = ['getit-backend-95de.onrender.com' , 'getit-backend-production.up.railway.app' , '127.0.0.1']
 
 
 # Application definition
